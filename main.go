f09f7e9e142e632ba8eb29902bf9922dc591b4bb
A. Diamond <diamond@sheprador.com>
2023-04-04 16:26:37 -0400

    Moved files up one directory

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/APTrust/partner-tools/aptrust/cmd"
+import "github.com/APTrust/partner-tools/cmd"
 
 func main() {
 	cmd.Execute()
